1d51dee9d95f6cd08f9e5375d27fc95dd96f6a5b
ibrahim akil <brhimakil.1@gmail.com>
2025-07-24 22:38:20 +0300

    added

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact({
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { pragma: 'h' }]
+        ]
+      }
+    })
+  ],
   esbuild: {
     target: 'es2020',
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    logOverride: { 
+      'this-is-undefined-in-esm': 'silent',
+      'empty-import-meta': 'silent'
+    },
+    loader: {
+      '.tsx': 'jsx',
+      '.ts': 'js'
+    }
   },
   build: {
     target: 'es2020',
@@ -14,11 +29,19 @@ export default defineConfig({
       onwarn() {
         return;
       }
-    }
+    },
+    assetsDir: 'assets',
+    sourcemap: false
   },
   server: {
     hmr: {
       overlay: false
+    }
+  },
+  resolve: {
+    alias: {
+      'react': 'preact/compat',
+      'react-dom': 'preact/compat'
     }
   }
 })
